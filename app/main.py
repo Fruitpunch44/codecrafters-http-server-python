3269f0ecd2d2b4a7bd4c4ff976c78d7e751a45af
Fruitpunch44 <gamerwar92@gmail.com>
2024-09-23 19:44:13 +0100

    update

--- app/main.py
+++ app/main.py
@@ -7,9 +7,28 @@ def main():
     # print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
+    IP = '127.0.0.1'
+    PORT = 4221
 
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    '''
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)'''
+    server_socket=socket.create_server((IP,4221),reuse_port=True)
+    server_socket.bind((IP, PORT))
+    server_socket.listen(5)
+
+    print(f'{IP} waiting for connections on PORT {PORT}')
+
+    while True:
+        client_soc, client_addr = server_socket.accept()
+        print(f'connected to {PORT} ON {client_addr}')
+        response = 'HTTP/1.1 200 OK\r\n\r\n'
+        client_soc.send(response.encode())
+
+        response = client_soc.recv(1024).decode()
+        if not response:
+            print('no response gotten')
+            break
 
 
 if __name__ == "__main__":
