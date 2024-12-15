ec36784f707c860521241f1e001a169a2379c76c
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-15 11:40:20 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -10,12 +10,17 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     while True:
-        client_sock,client_addr= server_socket.accept()
+        client_sock, client_addr = server_socket.accept()
         print(f'{client_sock} connected to port')
 
-        response=b"HTTP/1.1 200 OK\r\n\r\n"
+        response = b"HTTP/1.1 200 OK\r\n\r\n"
         client_sock.send(response)
 
+        request = client_sock.recv(1024).decode().split(" ")
+        if request[1] == "/":
+            client_sock.send(b'HTTP/1.1 200 OK\r\n\r\n')
+        else:
+            client_sock.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
 
 
 if __name__ == "__main__":
