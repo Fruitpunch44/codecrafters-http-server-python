0874a9a97896c750f325ac6853612e80079f8c36
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-15 12:33:35 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ def main():
             client_sock.send(b'HTTP/1.1 404 Not Found\r\n\r\n')
         elif request[1].split(' ') == "/":
             client_sock.sendall(response)
-        elif request.startswith('/echo/'):
+        elif request[1].startswith('/echo'):
             client_sock.sendall(response)
 
         else:
