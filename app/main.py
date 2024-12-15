f9e7b6f2db85999ca2cc4d2f43b04bf0c1739b69
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-15 12:48:35 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -14,9 +14,9 @@ def main():
         client_sock, client_addr = server_socket.accept()
         print(f'{client_sock} connected to port')
 
-        request = client_sock.recv(4096).decode()
+        request = client_sock.recv(4096).decode().split()
         # debugging purposes
-        print(f'{request.split()}')
+        print(f'{request[1]}')
         response = b'HTTP/1.1 200 OK\r\n\r\n'
 
         # exit loop if no request is gotten
