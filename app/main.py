01617d7b1b9711e4f350948801e3fd73878d3c43
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-16 13:04:33 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ def main():
         elif request[1].startswith('/echo/'):
             value = request[1][6:].strip()
             print(len(value))
-            res = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:f{len(value)}\r\n\r\n{value}'
+            res = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{len(value)}\r\n\r\n{value}'
             client_sock.send(res.encode())
 
         else:
