47357aab2fbee82fb5171ce80b173fdd9e09c570
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-16 22:39:17 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ def parse_request(request):
         try:
             directory = sys.argv[2]
             filename = 'foo.txt'
-            print(f'{directory}{filename}')
+            print(f'lmao{directory}{filename}')
             with open(f"/{directory}/{filename}", 'r') as file:
                 content = file.read()
                 res =f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(content)}\r\n\r\n{content}"
