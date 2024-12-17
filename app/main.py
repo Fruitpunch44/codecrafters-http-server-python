f809242cc607f132135eec6950c398da685e7fa7
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-17 13:29:11 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -75,7 +75,7 @@ def parse_request(request):
         path = request[1]
         print(path)
         files = " ".join(request[9:])  # read the data being sent by the post request
-        file_path = f'{directory}/{path}'
+        file_path = f'{directory}{path}'
         print(file_path)
 
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
