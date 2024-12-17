ab549afab47dd044124c7023bb099b80e4c87cb8
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-17 13:57:25 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -78,7 +78,7 @@ def parse_request(request):
         file_path = os.path.join(directory,path)
         print(file_path)
         os.mkdir(os.path.dirname(file_path))
-        with open(file_path,'w') as file:
+        with open(f"{file_path}.txt",'w') as file:
             new = files.lstrip('/').split('_')
             print(new)
             string = " ".join(new)
