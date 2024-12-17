c67b46b24053bb06e205c858be4f1536f3e53403
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-17 13:34:14 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -77,9 +77,8 @@ def parse_request(request):
         files = " ".join(request[9:])  # read the data being sent by the post request
         file_path = f'{directory}{path}'
         print(file_path)
-
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, 'w') as file:
+        with open(f'{directory}{path}', 'w') as file:
             new = files.lstrip('/').split('_')
             print(new)
             string = " ".join(new)
