5d1483a638134eece31752d8bb534b78f6ff551a
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-17 13:32:01 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -72,6 +72,7 @@ def parse_request(request):
 
     elif request[0].startswith("POST"):
         directory = sys.argv[2]
+        print(directory)
         path = request[1]
         files = " ".join(request[9:])  # read the data being sent by the post request
         file_path = os.path.join(directory, path)
