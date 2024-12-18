ca50b8e6f8179f277025ee83d297df61c6938707
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-18 11:45:15 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -37,10 +37,9 @@ def parse_headers(request):
         if ':' in field:
             key, value = field.split(":", 1)
             headers[key.strip()] = value.strip()
-            return headers
     for key, value in headers.items():
         print(f'{key}:{value}')
-
+        return headers
 
 
 def parse_request(request):
