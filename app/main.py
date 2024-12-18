34477d41ac22aad2e4362af75034dffc3ee63034
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-18 23:30:19 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -43,6 +43,7 @@ def parse_request(request):
     # not optimal but a path is usally on index 1
     global File_dir
     print(request)
+    print(request.split[0])
     path = 1
     if request[path] == "/" and request[0] == 'GET':
         return 'HTTP/1.1 200 OK\r\n\r\n'
@@ -59,7 +60,7 @@ def parse_request(request):
         res = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{len(value)}\r\n\r\n{value}'
         return res
 
-    elif request[path].startswith('/files/') and request.split[0]== 'GET':
+    elif request[path].startswith('/files/') and request.split[0] == 'GET':
         # it's reading from their servers not my local computer fyml
         try:
             directory = sys.argv[2]
