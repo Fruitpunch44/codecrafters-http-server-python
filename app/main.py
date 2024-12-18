f479fb1e2449593ca850a500fbd207c6afc53a79
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-18 12:34:58 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def handle_client(client_sock):
         print(f'{request}\n'
               f'{request[1]} \n '
               f'{request[1][6:]}\n')
-        client_sock.send(accept_gzip(request))
+        client_sock.send(accept_gzip(request).encode())
 
 
 def parse_headers(request):
