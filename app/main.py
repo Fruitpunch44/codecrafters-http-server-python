5f50bee2e2a0d2c1701bafe0c47da50fff1ae3f4
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-18 11:35:13 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ def handle_client(client_sock):
         response = parse_request(request)
         client_sock.send(response.encode())
         parse_headers(request)
+        accept_gzip(request)
 
 
 def parse_headers(request):
@@ -39,8 +40,9 @@ def parse_headers(request):
         if ':' in field:
             key, value = field.split(":", 1)
             headers[key] = value
-    for key,value in headers.items():
+    for key, value in headers.items():
         print(f'{key}:{value}')
+    return headers
 
 
 def parse_request(request):
@@ -78,14 +80,14 @@ def parse_request(request):
         directory = File_dir
         print(directory)  # debugging
         path = request[1][6:].strip('/')
-        print(path)# debugging
+        print(path)  # debugging
         files = " ".join(request[9:])  # read the data being sent by the post request
         file_path = f'{directory}{path}'
         print(file_path)  # debugging
         try:
             with open(file_path, 'w') as file:
                 new = files.lstrip('/').split('_')
-                print(new) # debugging
+                print(new)  # debugging
                 string = " ".join(new)
                 print(string)
                 print(len(string))
@@ -100,6 +102,12 @@ def parse_request(request):
 def show_clients():
     for x, y in enumerate(connected_client):
         print(f'{x + 1}:{y}')
+
+
+def accept_gzip(request):
+    head = parse_headers(request)
+    for key, value in head.items():
+        print(f'{key}:{value}')
 
 
 def main():
