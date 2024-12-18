0e9033413a887ab2a0b14a31e26ab0ddbd8e49a5
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-18 23:39:29 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -45,16 +45,18 @@ def parse_request(request):
     print(request)
     print(request[1])
     print(request.split()[0])
+    print(request.split()[1])
+
 
     path = 1
     if request[path] == "/" and request[0] == 'GET':
         return 'HTTP/1.1 200 OK\r\n\r\n'
 
-    elif request[path].startswith('/echo/') and request.split()[0] == 'GET':
+    elif request.split()[path] == '/echo/' and request.split()[0] == 'GET':
         value = request[1][6:]
         gzip = accept_gzip(request)
         res = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:{len(value)}\r\n\r\n{value}'
-        return gzip or re
+        return gzip or res
 
 
     elif request[path].startswith('/user-agent') and request.split()[0] == 'GET':
