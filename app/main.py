99b8e359b4f690fdf8d2a17bcb8130f12f916eab
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-18 11:50:59 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -22,9 +22,7 @@ def handle_client(client_sock):
         if not request:
             break
         # debugging purposes
-        print(f'{request}\n'
-              f'{request[1]} \n '
-              f'{request[1][6:]}\n')
+        print(f'{request}')
         print(parse_headers(request))
 
 
