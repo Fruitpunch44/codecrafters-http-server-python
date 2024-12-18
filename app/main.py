d41b27a45cb9a29e398efe29b9ee9af8737be2f6
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-18 11:23:46 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ import argparse
 
 connected_client = []
 lock = threading.Lock()
-File_dir=''
+File_dir = ''
+
 
 def handle_client(client_sock):
     with lock:
@@ -43,7 +44,7 @@ def parse_headers(request):
 
 def parse_request(request):
     # not optimal but a path is usally on index 1
-    global  File_dir
+    global File_dir
     path = 1
     if request[path] == "/" and request[0] == 'GET':
         return 'HTTP/1.1 200 OK\r\n\r\n'
@@ -75,7 +76,7 @@ def parse_request(request):
     elif request[0].startswith("POST"):
         directory = File_dir
         print(directory)  # debugging
-        path = request[1].strip('/')
+        path = request[1][6:].strip('/')
         print(path)
         files = " ".join(request[9:])  # read the data being sent by the post request
         file_path = f'{directory}{path}'
@@ -104,14 +105,13 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    parser=argparse.ArgumentParser('a simple http server')
+    parser = argparse.ArgumentParser('a simple http server')
     parser.add_argument("-d", "--directory")
-    args=parser.parse_args()
+    args = parser.parse_args()
 
     if 'directory' in args:
         global File_dir
-        File_dir=args.directory
-
+        File_dir = args.directory
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
