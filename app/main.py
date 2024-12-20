e8ddb9e5112a0381f189da31a5430b91c6865e86
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-20 23:01:24 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -120,7 +120,7 @@ def accept_gzip(request):
     if 'Accept-Encoding' in headers:
         if 'gzip' in headers['Accept-Encoding']:
             response = gzip.compress(body.encode('utf-8'))
-            res = f'HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\n\r\nContent-Length: {len(body)}\r\n\r\n' + response.decode('latin1')
+            res = f'HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\n\r\nContent-Length: {len(response)}\r\n\r\n' + response.decode('latin1')
             return res
 
     return None
