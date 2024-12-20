0b27e643f98436e8775119438d606d8d5495b7ed
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-20 21:33:54 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -106,7 +106,7 @@ def show_clients():
 
 def accept_gzip(request):
     headers = parse_headers(request)
-    if 'Accept-Encoding' in headers
+    if 'Accept-Encoding' in headers:
         res = 'HTTP/1.1 200 OK\r\nContent-Encoding:gzip\r\n\r\n'
         return res
 
