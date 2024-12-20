57cd7c05e08f03757d29c1b5f541ddae5713af69
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-20 22:15:53 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ def parse_request(request):
     print(request[1])
     print(request.split())
     print(request.split()[0])
-    print(request.strip()[1][6:])
+    print(request.split()[1][6:].strip('/'))
 
     path = 1
     filename = request[1][6:]
