0dc06d0f314781f9521bc54c54331fce6a4b2728
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-20 23:59:40 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -123,6 +123,7 @@ def accept_gzip(request):
             print(f'decoded:{compressed.hex()}')
             print(f'length_of_compressed:{len(compressed)}')
             compressed_data=compressed.decode('latin1')
+            print(compressed_data)
             res = (
                       'HTTP/1.1 200 OK\r\n'
                       'Content-Encoding: gzip\r\n'
