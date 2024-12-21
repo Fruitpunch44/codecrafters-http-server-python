1e1708d9f90930e495766f2a9b387335040de9e5
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-21 10:02:21 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -113,14 +113,12 @@ def accept_gzip(request):
             print(f'compressed_message={compressed}')
             print(f'decoded:{compressed.hex()}')
             print(f'length_of_compressed:{len(compressed)}')
-            compressed_data = str(compressed)
-            print(f'compressed data:{compressed_data}')
             res = (
                 'HTTP/1.1 200 OK\r\n'
                 'Content-Encoding: gzip\r\n'
                 f'Content-Length: {len(compressed)}\r\n'
                 '\r\n'
-                f'{compressed_data}'
+                f'{compressed}'
             )
             print(f"response to send: {res}")
             return res
