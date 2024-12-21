1f098a9477f5bac789560a82d53479c1a463f778
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2024-12-21 13:07:16 +0100

    [any message]

--- app/main.py
+++ app/main.py
@@ -118,7 +118,7 @@ def accept_gzip(request):
                 'Content-Encoding: gzip\r\n'
                 f'Content-Length: {len(compressed)}\r\n'
                 '\r\n'
-                f'{compressed.decode()}'
+                f'{compressed.decode('latin1')}'
             )
             print(f"response to send: {res}")
             return res
